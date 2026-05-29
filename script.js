07640c945cb27c76936e4ce6407afb88d6d1a75d
buranshettydharmateja-a11y <buranshettydharmateja@gmail.com>
2026-05-29 16:08:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const gifStages = [
 ]
 
 const noMessages = [
-    "Thanks bujji",
+    "you love me🤩",
     "Are you positive? 🤔",
     "Pookie please... 🥺",
     "If you say no, I will be really sad...",
